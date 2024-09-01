cec8c8e9846cbd89ba6645bfd29c4c4c16ee8fbe
chandrakant <chandrakantnagwanshi97@gmail.com>
2024-09-01 14:00:17 +0530

    add navbar

---
+++ navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ fontSize: "20px", color: "black" }}>Logo</div>
+      <ul>
+        <li>HOME</li>
+        <li>Features</li>
+        <li>Product</li>
+      </ul>
+    </nav>
+  );
+}
